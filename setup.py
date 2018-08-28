5adf1e08923835efc20fbb4ef6e0c7e6020b3fde
Znerual <laurenz_ruzicka@gmx.at>
2018-08-28 11:06:53 +0200

    Initialisation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name='kullback-leibner-divergence-criterion',
+    version=0.1,
+    url='https://github.com/Znerual/Kullback-Leibner-divergence-Criterion',
+    author='Laurenz Ruzicka',
+    author_email='laurenz.ruzicka@tuwien.student.ac.at',
+    ext_modules=cythonize('kullback_leibner_divergence_criterion.pyx'),
+    include_dirs=[numpy.get_include()]
+)
