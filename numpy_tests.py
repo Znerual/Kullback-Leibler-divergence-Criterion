168171344598b585e4e83c233e66b1f43a07e357
Znerual <laurenz_ruzicka@gmx.at>
2018-08-29 14:32:21 +0200

    Adjusted weights

--- numpy_tests.py
+++ numpy_tests.py
@@ -1,10 +1,9 @@
 import numpy as np
 from sklearn.datasets import make_gaussian_quantiles
 
-#Construct dataset
-#generate Values
-X1,y1 = make_gaussian_quantiles(mean=(2, 2), cov=1.5, n_samples=4, n_features=2, n_classes=1, random_state=1)
-X2,y2 = make_gaussian_quantiles(cov=1.2, n_samples=3, n_features=2, n_classes=1, random_state=1)
+#Dataset creation
+X1,y1 = make_gaussian_quantiles(mean=(2, 2), cov=1.5, n_samples=800, n_features=2, n_classes=2, random_state=1)
+X2,y2 = make_gaussian_quantiles(cov=1.2, n_samples=600, n_features=2, n_classes=2, random_state=1)
 
 X12 = np.concatenate((X1,X2))
 X = np.concatenate((X12,X12))
@@ -18,16 +17,24 @@ y = np.concatenate((y1,y2))
 #Generating weights
 n_alt_hyp_size = len(X1) #BSM
 n_hyp_size = len(X2) #SM
+
 #in case of y=0, SM
 w_alt_hyp = np.zeros(n_alt_hyp_size)
+w_alt_hyp += 0.01
 w_hyp = np.ones(n_hyp_size)
+w_hyp /= n_hyp_size
 w0 = np.concatenate((w_alt_hyp, w_hyp))
+
 #in case of y=1, BSM
 w_alt_hyp = np.ones(n_alt_hyp_size)
+w_alt_hyp /= n_alt_hyp_size
 w_hyp = np.zeros(n_hyp_size)
+w_hyp += 0.01
 w1 = np.concatenate((w_alt_hyp, w_hyp))
+
 #final weights
 w = np.concatenate((w0,w1))
+
 print "X mit " +str(len(X)) + " Eintraegen "
 print X
 print "y mit " + str(len(y)) + " Eintraegen "
