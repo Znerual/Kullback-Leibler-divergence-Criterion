4fce975e35cbffcd0b1c6e0c9e0a906f0c31f670
Znerual <laurenz_ruzicka@gmx.at>
2018-09-10 15:20:08 +0200

    Beautify and comment code

--- pandas_kule.py
+++ pandas_kule.py
@@ -33,7 +33,7 @@ argParser.add_argument('--boost_algorithm', action='store', default='SAMME', nar
 args = argParser.parse_args()
 
 #Set the version of the script
-vversion = 'v7'
+vversion = 'v8'
 
 #set criterion, you can choose from (gini, kule, entropy, hellinger)
 
@@ -41,10 +41,11 @@ vversion = 'v7'
 import RootTools.core.logger as Logger
 logger = Logger.get_logger(args.logLevel, logFile = None)
 
-
+#Kule algorithm
 from kullback_leibner_divergence_criterion import KullbackLeibnerCriterion
 kldc = KullbackLeibnerCriterion(1, np.array([2], dtype='int64'))
 
+#setting up the file save name
 version = vversion
 if args.small:
     args.data_version += '_small'
@@ -52,6 +53,7 @@ if args.small:
 if args.log_plot:
     version += '_log'
 version += '_' + args.criterion
+
 #find directory
 input_directory = os.path.join(tmp_directory, args.data_version)
 logger.debug('Import data from %s', input_directory)
@@ -65,6 +67,7 @@ elif args.criterion == 'entropy':
     dt = DecisionTreeClassifier(max_depth= args.max_depth, criterion='entropy')
 else:
     assert False, "You choose the wrong Classifier"
+
 # Create and fit an AdaBoosted decision tree
 bdt = AdaBoostClassifier(dt,
                          algorithm= args.boost_algorithm,
@@ -75,11 +78,11 @@ df = pd.read_hdf(os.path.join(input_directory, args.data))
 
 X1 = np.array(df[['genZ_pt/F','genZ_cosThetaStar/F']])
 X = np.concatenate((X1,X1))
-
+    #generate targets
 y0 = np.zeros(len(X1))
 y1 = np.ones(len(X1))
 y = np.concatenate((y0,y1))
-
+    #read weights
 w0 = np.array(df['sm_weight'])
 sm_weight_mean = np.mean(w0)
 w0 /= sm_weight_mean
@@ -87,7 +90,7 @@ w1 = np.array(df['bsm_weight'])
 bsm_weight_mean = np.mean(w1)
 w1 /= bsm_weight_mean
 w = np.concatenate((w0,w1))
-
+    #calculate weight mean and stretch it to an array
 weight_mean = np.mean([sm_weight_mean, bsm_weight_mean])
 weight_mean_array = np.full([len(w0)], weight_mean)
 
@@ -97,42 +100,41 @@ logger.info('Mean of sm_weights: %f, mean of bsm_weights: %f',sm_weight_mean, bs
 bdt.fit(X, y, w)
 
 
-
-#get the directory
+#get the directory for data
 output_dir = os.path.join(tmp_directory, args.data_version)
 if not os.path.exists(output_dir):
     os.makedirs( output_dir)
 
-#save to file
+#save the data to file
 if args.save:
     logger.info("Save the trained plot to %s, it uses the %s criterion", output_dir, args.criterion)
     joblib.dump(bdt,  os.path.join(output_dir,"bdt-trainingdata-" + version)) 
     logger.info("Dumped the tree to %s",  os.path.join(output_dir,"bdt-trainingdata-" + version))
 
-#export the tree
+#export the tree TODO! NOT WORKING!!
 if args.export:
     export_graphviz(bdt, out_file= os.path.join(output_dir, version + "-tree.dot"))
     logger.info("Exported the tree as .dot to %s", os.path.join(output_dir, version + "-tree.dot")) 
 
-print('distance score: ', bdt.score(X, y))
 
-reached_score = bdt.score(X,y,w)
 #calculate the score
+reached_score = bdt.score(X,y,w)
 logger.info('Reached a score of %f',  reached_score)
 
+#end if no_plot argument was choosen
 if args.no_plot:
     raise SystemExit
 
-#get the output directory
+#get the output directory for plots
 output_directory = os.path.join(plot_directory,'Kullback-Leibner-Plots',  argParser.prog.split('.')[0], vversion)
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 logger.info('Save to %s directory', output_directory)
 
+#setup plot attributes
 plot_colors = "brk"
-plot_step = 0.5
+plot_step = 0.25
 class_names = ["SM","BSM","Event"]
-
 plt.figure(figsize=(12, 13))
 
 #Plot the decision boundaries
@@ -142,20 +144,16 @@ y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                      np.arange(y_min, y_max, plot_step))
 
-#Z = bdt.predict(np.c_[xx.ravel(), yy.ravel()])
-
 #fill the plot with gradient color
-Z = bdt.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = bdt.decision_function(np.c_[xx.ravel(), yy.ravel()]) #use bdt.predict for non gradient filling
 Z = Z.reshape(xx.shape)
 plt.pcolormesh(xx,yy,Z, cmap=plt.cm.coolwarm)
-#cs = plt.contour(xx, yy, Z, cmap=plt.cm.coolwarm)
-#cs = plt.contour(xx, yy, Z, linewidths=0.75, colors='k') #draw edges
-cs = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm) #plt.contour(.., linewidth=0.75, colors='k') draws edges
 plt.axis("tight")
+
 #Plot the training points
 for i, n, c in zip(range(2), class_names[:2], plot_colors[:2]):
-    #idx = np.intersect1d(np.where(y == i),np.where(w > w_min))
-    idx = np.where(y == i)
+    idx = np.where(y == i) #to filter weights: np.intersect1d(np.where(y == i), np.where(...))
     plt.scatter(X[idx, 0], X[idx, 1],
                 c=c, cmap=plt.cm.coolwarm,
                 s=20, edgecolor='k',
@@ -168,12 +166,10 @@ plt.xlabel('p_T(T) (GeV)')
 plt.ylabel('Cos(Theta*)')
 plt.title('Decision Boundary')
 
-# Plot the two-class decision scores
-#twoclass_output = bdt.decision_function(X[np.where(w> w_min)])
-
-#Plot the Histogramm for the number of Events over genZ_p..
+#make a list for iterating over different weights
 plot_weights = [w0,w1, weight_mean_array]
 
+#Plot the Histogramm for the number of Events over genZ_p weighted with sm and bsm..
 plt.subplot(221)
 plot_range = (df['genZ_pt/F'].min(), df['genZ_pt/F'].max())
 for i, n, c in zip(range(3), class_names, plot_colors):
@@ -191,30 +187,18 @@ plt.xlabel('p_T(Z) (GeV)')
 plt.title('Weighted p_T(Z)')
 plt.legend(loc='upper right')
 
-#prints the predicted value
-#print bdt.decision_function([[400.0,0.0]])
-
-#prints the predicted class
-#print bdt.predict([[400.0,0.0]])
-
-#Plot the Feature Importance..
 
 #Plot the decision diagram
 score  = bdt.decision_function(X)
-#now, we weight our score
-#plot_range = (min(np.amin(score[:len(score)/2]*w0),np.amin(score[len(score)/2:]*w1)  ), 
- #       max(np.amax(score[:len(score)/2]*w0),np.amax(score[len(score)/2:]*w1   )))
 plot_range = (score.min(), score.max())
 plt.subplot(222)
 for i, n, c in zip(range(2), class_names[:2], plot_colors[:2]):
-    #plt.hist(twoclass_output[y[np.where(w> w_min)] == i],
     plt.hist(score[i*len(X)/2:(i+1)*len(X)/2] ,
              bins=10,
              range=plot_range,
              facecolor=c,
              weights=plot_weights[i],
              label='Class %s' % n,
-            # label='Score',
              alpha=.5,
              edgecolor='k')
     x1, x2, y1, y2 = plt.axis()
@@ -228,8 +212,7 @@ plt.tight_layout()
 plt.subplots_adjust(wspace=0.35)
 
 
-#Draw the same plots with Theta Star
-
+#Draw the same event plot with Theta Star insted of ptz
 plt.subplot(223)
 plot_range = (df['genZ_cosThetaStar/F'].min(), df['genZ_cosThetaStar/F'].max())
 for i, n, c in zip(range(3), class_names, plot_colors):
