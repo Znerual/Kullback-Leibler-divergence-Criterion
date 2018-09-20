83bf67bf1d8edcc9243a1a663c4b58f2f8b26ddb
Znerual <laurenz_ruzicka@gmx.at>
2018-09-20 12:10:40 +0200

    Added comments

--- gen_datasets.py
+++ gen_datasets.py
@@ -36,10 +36,12 @@ def gauss_easy(n_samples_bsm, n_samples_sm):
 
     #final weights
     w = np.concatenate((w0,w1))
-
+    
+    #calculate the minimal weights, to avoid division by zero
     w_min = min((2.0 * EPSILON) / n_hyp_size, (2.0 * EPSILON) / n_alt_hyp_size)
 
     return X, y, w, w_min
+
 
 def gauss_hard(n_samples_bsm, n_samples_sm):
     #Dataset creation
@@ -48,11 +50,10 @@ def gauss_hard(n_samples_bsm, n_samples_sm):
 
     X12 = np.concatenate((X1,-X2 + 1))
     X = np.concatenate((X12,X12))
-    n_samle_size = len(X12)
 
     # Generating labels
-    y1 = np.zeros(n_samle_size)
-    y2 = np.ones(n_samle_size)
+    y1 = np.zeros(len(X12))
+    y2 = np.ones(len(X12))
     y = np.concatenate((y1,y2))
 
     #Generating weights
@@ -77,6 +78,7 @@ def gauss_hard(n_samples_bsm, n_samples_sm):
     #final weights
     w = np.concatenate((w0,w1))
 
+    #calculate the minimal weights, to avoid division by zero
     w_min = min((2.0 * EPSILON) / n_hyp_size, (2.0 * EPSILON) / n_alt_hyp_size)
 
     return X, y, w, w_min
@@ -85,59 +87,48 @@ def exponential_easy(n_samples_bsm, n_samples_sm):
     assert False, "exponential function is not ready to use, crashes the whole pc"
     lambda_sm = 2.0
     lambda_bsm = 1.5
-
+    #generate the values from the exponential distributions
     X1 = make_exponential_lambda(lambda_bsm, n_samples_bsm)
     X2 = make_exponential_lambda(lambda_sm, n_samples_sm)
-
+    
+    #build the event set
     X12 = np.concatenate((X1,X2))
     X = np.concatenate((X12,X12))
-    n_samle_size = len(X12)
 
     # Generating labels
-    y1 = np.zeros(n_samle_size)
-    y2 = np.ones(n_samle_size)
+    y1 = np.zeros(len(X12))
+    y2 = np.ones(len(X12))
     y = np.concatenate((y1,y2))
 
-    #Generating weights
-    #EPSILON = 0.00001 / (n_samples_sm + n_samples_bsm)
-    #in case of y=0, SM
-    #alt_hyp = bsm
+    #Generating weights ,in case of y=0, SM, alt_hyp = bsm, prepare the weight arrays
     w_alt_hyp = np.zeros(len(X1))
     w_hyp = np.zeros(len(X2))
+
+    #looper over the events and calculate the weights
     for i in range(0,len(X1)):
         w = np.sqrt(calc_exponential_lambda(lambda_bsm, X1[i][0]) * calc_exponential_lambda(lambda_bsm, X1[i][1]))
-#Wurzel"!""
-        w_alt_hyp[i] = w / (lambda_bsm) # * weight_increase)
-    for i in range(0, len(X2) -1):
+        w_alt_hyp[i] = w / (lambda_bsm) 
+    for i in range(0, len(X2)):
         w = np.sqrt(calc_exponential_lambda(lambda_sm, X2[i][0]) * calc_exponential_lambda(lambda_sm, X2[i][1]))
-        w_hyp[i] = w / (lambda_sm) # * weight_increase)
-    #w_alt_hyp /= lambda_bsm
-    #w_hyp /= lambda_sm
+        w_hyp[i] = w / (lambda_sm)
     w0 = np.concatenate((w_alt_hyp, w_hyp))
 
-    #in case of y=1, BSM
-    #alt_hyp = sm
+    #in case of y=1, BSM, alt_hyp = sm
     w_alt_hyp = np.zeros(len(X2))
     w_hyp = np.zeros(len(X1))
 
-    for i in range(0,len(X2) -1):
+    #loop over the events and calculate the weights
+    for i in range(0,len(X2)):
         w = np.sqrt(calc_exponential_lambda(lambda_sm, X2[i][0] ) * calc_exponential_lambda(lambda_sm, X2[i][1] ))
         print "calc e l" + str(calc_exponential_lambda(lambda_sm, X2[i][0]))
         w_alt_hyp[i] = w / (lambda_sm) # * weight_increase)
-    for i in range(0, len(X1) -1):
+    for i in range(0, len(X1)):
         w = np.sqrt(calc_exponential_lambda(lambda_bsm, X1[i][0] ) * calc_exponential_lambda(lambda_bsm, X1[i][1] ))
         w_hyp[i] = w / (lambda_bsm) # * weight_increase)
-    #w_alt_hyp /= lambda_sm
-    #w_hyp /= lambda_bsm
-
     w1 = np.concatenate((w_alt_hyp, w_hyp))
 
     #final weights
     w = np.concatenate((w0,w1))
-
-    w_min = np.amin(w[np.where(w > 0.0)]) #+ EPSILON
-    EPSILON = w_min / 100.0
-    w[np.where(w == 0.0)] += EPSILON
 
     print "X"
     print X
@@ -152,4 +143,3 @@ def make_exponential_lambda(l, n_samples):
     return np.random.exponential(l, (n_samples,2))
 def calc_exponential_lambda(l,x):
     return (l * np.exp(-l * x))
-#exponential_easy(4,4)
