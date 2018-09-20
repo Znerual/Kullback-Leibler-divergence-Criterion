ba6a2af420dbd5a3961437136184ed99204a4424
Znerual <laurenz_ruzicka@gmx.at>
2018-09-20 10:06:49 +0200

    Corrected the 0-check with a round at the 6th position

--- criterion.py
+++ criterion.py
@@ -7,25 +7,29 @@ class KullbackLeibler:
         self.logger = logger
     
     def kule_div(self, sm, bsm, start_index_offset = 0):
+        #checking for correct input
         assert sm.GetNbinsX() == bsm.GetNbinsX(), "Different Bin counts, in criterion.py"
         assert start_index_offset < sm.GetNbinsX(), "The offsetvalue is too big"
+          
         kule = 0
         varianz = 0
+        #loop over all the selected histo bins
         for i in range(1 + start_index_offset,bsm.GetNbinsX()+1):
             p = bsm.GetBinContent(i)
             q = sm.GetBinContent(i)
             w_bsm = bsm.GetBinError(i)
             w_sm = sm.GetBinError(i)
-            if not (p ==  0.0 and q == 0.0):
-                with np.errstate(divide='raise'):
-                    try:
-                        lg = np.log(p/q)
-                        kule += 4.0 * p * lg
-                        varianz += np.square(w_bsm) * (np.square(1.0 + lg)) + np.square(p * w_sm/ q) #nicht error nennen, varianz
-                    except ZeroDivisionError:                    
-                        self.logger.warning("Divided by zero np.log(%f/%f), at bin %i with bin SM Error %f and BSM error %f", p,q, i, w_sm, w_bsm) 
-                    except FloatingPointError:
-                        self.logger.warning("Floating Point Error np.log(%f/%f) at bin %i with error bins SM: %f and BSM: %f", p,q, i, w_sm, w_bsm) 
+            #round, to avoid 0 != 0 
+            if round(p,6) ==  0.0 and round(q,6) == 0.0: continue
+            with np.errstate(divide='raise'):
+                try:
+                    lg = np.log(p/q)
+                    kule += p * lg
+                    varianz += np.square(w_bsm) * (np.square(1.0 + lg)) + np.square(p * w_sm/ q) #nicht error nennen, varianz
+                except ZeroDivisionError:                    
+                    self.logger.warning("Kule Divided by zero np.log(%f/%f), at bin %i with bin SM Error %f and BSM error %f", p,q, i, w_sm, w_bsm) 
+                except FloatingPointError:
+                    self.logger.warning("Kule Floating Point Error np.log(%f/%f) at bin %i with error bins SM: %f and BSM: %f", p,q, i, w_sm, w_bsm) 
                      
         return kule, np.sqrt(varianz)#return std (wurzel)
 class Gini:
@@ -41,13 +45,13 @@ class Gini:
             q = sm.GetBinContent(i)
             w_bsm = bsm.GetBinError(i)
             w_sm = sm.GetBinError(i)
-            if not (p ==  0.0 and q == 0.0):
-                with np.errstate(divide='raise'):
-                    try:
-                        gini += (p / np.sqrt(p + q))
-                        varianz_nenner = 4.0 * (p + q)**3
-                        varianz += ((p + 2.0*q) * w_bsm) **2 / varianz_nenner  + (p * w_sm)**2 / varianz_nenner
-                    except FloatingPointError:
-                        self.logger.warning("Floating Point Error BSM bin: %f, SM bin: %f,  at bin %i with error bins SM: %f and BSM: %f", p,q, i, w_sm, w_bsm) 
+            if  (round(p,7) ==  0.0 and round(q,7) == 0.0): continue
+            with np.errstate(divide='raise'):
+                try:
+                    gini += (p / np.sqrt(p + q))
+                    varianz_nenner = 4.0 * (p + q)**3
+                    varianz += ((p + 2.0*q) * w_bsm) **2 / varianz_nenner  + (p * w_sm)**2 / varianz_nenner
+                except FloatingPointError:
+                    self.logger.warning("Gini Floating Point Error BSM bin: %f, SM bin: %f,  at bin %i with error bins SM: %f and BSM: %f", p,q, i, w_sm, w_bsm) 
         
         return gini, np.sqrt(varianz)
